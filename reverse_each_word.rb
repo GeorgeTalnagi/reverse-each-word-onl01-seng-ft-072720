5ab4423f881b2fadc18a100ad7b2c55cbf7a222a
George Talnagi <gtalnagi@yahoo.com>
2020-07-31 19:46:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
 def reverse_each_word(sentence)
-  sentence.split().collect{|word| word.reverse}.join(" ")
+  new_sentence = []
+  sentence = sentence.split
+  sentence.collect do |word|
+  new_sentence << "#{word.reverse}"
+  end
+  new_sentence.join(" ")
 end
